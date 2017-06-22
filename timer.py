4ef714ca075cda1747d75576eeed8bb1972fe666
fvural <f_vural@hotmail.com>
2017-06-22 08:10:56 +0300

    asd

--- timer.py
+++ timer.py
@@ -1,5 +1,32 @@
+
+####################
+def cls():
+    import os
+    os.system('cls')
+    return
+cls()
 import time
 starttime=time.time()
+
+#sıcaklık,nem,lokasyon,tarih+saat
 while True:
-  print ("tick")
-  time.sleep(60.0 - ((time.time() - starttime) % 60.0))
+  import sqlite3
+
+  baglanti = sqlite3.connect('veriler.db')
+  baglanti.row_factory = sqlite3.Row
+  veritabani_sec = baglanti.cursor()
+  # baglantikapatmavekaydetme.
+  import random
+  from datetime import datetime
+
+  adi = random.choice(['fatih', 'faruk', 'mehmet', 'serkan', 'ahmet'])
+  soyadi = random.choice(['fatih', 'faruk', 'mehmet', 'serkan', 'ahmet'])
+  no = random.randint(1, 99)
+  date = str(datetime.now())
+  sql = veritabani_sec.execute("INSERT INTO ogrenciler (no,adi,soyadi,date) VALUES(?,?,?,?)", (no, adi, soyadi, date))
+  print( no,adi, soyadi,date)
+  baglanti.commit()
+  baglanti.close()
+
+  time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+
