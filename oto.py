af3603ec023f3246cdd14c7cc24eb906f2b0b8ee
fvural <f_vural@hotmail.com>
2017-06-22 09:31:27 +0300

    değişim

---
+++ oto.py
@@ -0,0 +1,34 @@
+
+####################
+def cls():
+    import os
+    os.system('cls')
+    return
+cls()
+import time
+starttime=time.time()
+
+#sıcaklık,nem,lokasyon,tarih+saat,cihazno
+x=0
+while True:
+  import sqlite3
+
+  baglanti = sqlite3.connect('veriler.db')
+  baglanti.row_factory = sqlite3.Row
+  veritabani_sec = baglanti.cursor()
+  # baglantikapatmavekaydetme.
+  import random
+  from datetime import datetime
+
+  adi = random.choice(['fatih', 'faruk', 'mehmet', 'serkan', 'ahmet'])
+  soyadi = random.choice(['fatih', 'faruk', 'mehmet', 'serkan', 'ahmet'])
+  no = random.randint(1, 99)
+  date = str(datetime.now())
+  sql = veritabani_sec.execute("INSERT INTO ogrenciler (no,adi,soyadi,date) VALUES(?,?,?,?)", (no, adi, soyadi, date))
+  x=x+1
+  print( x,no,adi, soyadi,date)
+  baglanti.commit()
+  baglanti.close()
+
+  time.sleep(10.0 - ((time.time() - starttime) % 10.0))
+
