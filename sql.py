0927214cabb3d93dad4c5880d268da3958523490
fvural <f_vural@hotmail.com>
2017-06-23 17:42:11 +0300

    değişim

--- sql.py
+++ sql.py
@@ -4,20 +4,18 @@ con.row_factory=sqlite3.Row
 vt=con.cursor() #Veritabanında İşlem yapabilmek için Cursor oluştur
 
 
-def list(bak):
+def list(tablo):
 
-    print(bak)
+    print(tablo)
 
-    sq = 'select * from ogrenciler'
-    #kontrol = vt.execute(sq, (bak,))
-    #pw = kontrol.fetchone()
-    #kontrol = vt.execute("select * from ogrenciler", (bak))
+    sq = 'select * from ?'
+    kontrol = vt.execute(sq, (tablo,))
 
 
 
 
 
-bak = "ogrenciler"
-list(bak)
 
-kontrol = vt.execute('select * from ?', (bak,))
+bak = "tablo"
+list(ogrenciler)
+
